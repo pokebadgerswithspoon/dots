caf5679da66029f6b552ea6b4c97590061f44c79
Lauri <lauri@icantfeelmylegs.com>
2020-06-09 14:09:35 +0300

    sway-revvert sway-unrevert hacks to get rid of SCALE hacks for sway

---
+++ home/sway/.config/sway/tools/sway-revert.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+gsettings set org.gnome.desktop.interface scaling-factor 2
+gsettings set org.gnome.desktop.interface text-scaling-factor 0.87
+gsettings set org.gnome.desktop.interface gtk-theme "Greybird"
+gsettings set org.gnome.desktop.interface cursor-size 24
+gsettings set org.gnome.nautilus.icon-view default-zoom-level small
