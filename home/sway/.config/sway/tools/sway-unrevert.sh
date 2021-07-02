8ff27c7a8d8fe101e46b5dcb68a8c2303800fffd
Lauri <lauri@icantfeelmylegs.com>
2021-07-02 23:34:40 +0300

    Current progress

--- home/sway/.config/sway/tools/sway-unrevert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-gsettings set org.gnome.desktop.interface scaling-factor 1
-gsettings set org.gnome.desktop.interface text-scaling-factor 1.75
-gsettings set org.gnome.desktop.interface gtk-theme "GreybirdX2"
-gsettings set org.gnome.desktop.interface cursor-size 48
-gsettings set org.gnome.nautilus.icon-view default-zoom-level small
-
